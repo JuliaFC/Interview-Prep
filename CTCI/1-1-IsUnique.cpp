e24e980fc44a7997493a2de8782af94ebd8d13e3
JuliaFC <mjfc@cin.ufpe.br>
2019-11-05 17:06:33 -0300

    Updating to consider all ASCII characters and also check the string size for deciding on uniqueness or not.

--- CTCI/1-1-IsUnique.cpp
+++ CTCI/1-1-IsUnique.cpp
@@ -5,10 +5,12 @@ using namespace std;
 
 bool isUnique(string s) {
 
-vector<int> char_map(26, 0);
+vector<int> char_map(256, 0);
+
+if(s.size() > 256) return false;
 
 for(auto c : s) {
-    if(++char_map[c - 'a'] > 1) {
+    if(++char_map[c - '0'] > 1) {
         return false;
     }
 }
